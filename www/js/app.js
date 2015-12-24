5f5641afe08f2d8ae3bd9e6d42ae468cb542f977
eduardonalves <eduardonalves@gmail.com>
2015-12-24 20:31:24 -0200

    ajustes

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-var messagesRef = new Firebase('https://crackling-fire-8527.firebaseio.com/nome/');
+
 angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
