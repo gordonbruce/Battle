ab37ad82b1bff592c72440ea5ff3537841db81a5
Eduardo Alves <eduardonalves@gmail.com>
2015-12-24 15:01:17 -0200

    ajustes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -49,7 +49,7 @@ angular.module('app.controllers', [])
       var username = data.name || "anonymous";
       var message = data.text;
   
-      var messageText = "<a class='item item-avatar' href='#'><img src='http://ionicframework.com/img/docs/venkman.jpg'><h2>"+username+"</h2><p>"+message+"</p></a>";
+      var messageText = "<a class='item item-avatar' href='#'><img src='https://en.gravatar.com/avatar/717ee15a5d86c2b8eded5a80adc58e30.jpg?d=retro&size=80'><h2>"+username+"</h2><p>"+message+"</p></a>";
 
       //ADD MESSAGE
       messageList.append(messageText)
