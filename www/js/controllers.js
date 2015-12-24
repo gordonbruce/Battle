0c21765d3c7e9039797bb5731c8a0f9c5b63f303
eduardonalves <eduardonalves@gmail.com>
2015-12-24 19:38:13 -0200

    ajuste

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -22,42 +22,46 @@ angular.module('app.controllers', [])
    
 .controller('jogoCtrl', function($scope) {
 	 // CREATE A REFERENCE TO FIREBASE
-    var messagesRef = new Firebase('https://crackling-fire-8527.firebaseio.com/nome/');
+    setTimeout(function(){
+      alert('teste');
+       var messagesRef = new Firebase('https://crackling-fire-8527.firebaseio.com/nome/');
 
-    // REGISTER DOM ELEMENTS
-  //  var messageField = $('#messageInput');
-    //var nameField = $('#nameInput');
-    var messageList = $('#game-messages');
+        // REGISTER DOM ELEMENTS
+      //  var messageField = $('#messageInput');
+        //var nameField = $('#nameInput');
+        var messageList = $('#game-messages');
+       
+        // LISTEN FOR KEYPRESS EVENT
+      /*  messageField.keypress(function (e) {
+          if (e.keyCode == 13) {
+            //FIELD VALUES
+            var username = nameField.val();
+            var message = messageField.val();
+
+            //SAVE DATA TO FIREBASE AND EMPTY FIELD
+            messagesRef.push({name:username, text:message});
+            messageField.val('');
+          }
+        }); */
+
+        // Add a callback that is triggered for each chat message.
+        messagesRef.limitToLast(10).on('child_added', function (snapshot) {
+          //GET DATA
+          var data = snapshot.val();
+          var username = data.name || "anonymous";
+          var message = data.text;
+      
+          var messageText = '<a class="item item-avatar" href="#"><img src="https://en.gravatar.com/avatar/717ee15a5d86c2b8eded5a80adc58e30.jpg?d=retro&size=80"><h2>'+username+'</h2><p>'+message+'</p></a>';
+
+          //ADD MESSAGE
+          $('#game-messages').append(messageText);
+          $('#game-messages').scrollTop($('#game-messages')[0].scrollHeight);
+        
+
+        //  messageList[0].scrollTop = messageList[0].scrollHeight;
+
+        });
+    },10000);
    
-    // LISTEN FOR KEYPRESS EVENT
-  /*  messageField.keypress(function (e) {
-      if (e.keyCode == 13) {
-        //FIELD VALUES
-        var username = nameField.val();
-        var message = messageField.val();
-
-        //SAVE DATA TO FIREBASE AND EMPTY FIELD
-        messagesRef.push({name:username, text:message});
-        messageField.val('');
-      }
-    }); */
-
-    // Add a callback that is triggered for each chat message.
-    messagesRef.limitToLast(10).on('child_added', function (snapshot) {
-      //GET DATA
-      var data = snapshot.val();
-      var username = data.name || "anonymous";
-      var message = data.text;
-  
-      var messageText = '<a class="item item-avatar" href="#"><img src="https://en.gravatar.com/avatar/717ee15a5d86c2b8eded5a80adc58e30.jpg?d=retro&size=80"><h2>'+username+'</h2><p>'+message+'</p></a>';
-
-      //ADD MESSAGE
-      $('#game-messages').append(messageText);
-      $('#game-messages').scrollTop($('#game-messages')[0].scrollHeight);
-    
-
-    //  messageList[0].scrollTop = messageList[0].scrollHeight;
-
-    });
 })
  
